b3f6902693858f014be9bc8b8a7e59b4ddf70882
shaffkhan <shaffkhany@gmail.com>
2025-02-13 23:36:14 +0500

    minor change

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,7 +7,7 @@ const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/migrations/*.js'],
+  migrations: ['dist/src/migrations/*.js'],
   synchronize: false,
   migrationsTableName: 'migration',
   extra: {
