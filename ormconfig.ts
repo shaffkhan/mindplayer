65dcd17bc5fd92e3e78a5a7bdba03d2ada697c61
shaffkhan <shaffkhany@gmail.com>
2025-02-22 17:35:45 -0500

    add user preferences and change the user as well

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  url:"postgres://mindplayer_user:shaff@127.0.0.1:5432/mindplayer",
-  schema: "public", // Explicitly set the default schema
+  url: "postgres://mindplayer_user:shaff@127.0.0.1:5432/mindplayer",
+  schema: "mindplayer", // Explicitly set the schema to "mindplayer"
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/src/migrations/*.js'],
   synchronize: false,
